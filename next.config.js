e349562e0e6aec80a02f693e84b5eeb812b2dc47
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2024-01-01 12:03:47 +0330

    set env file

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+module.exports = {
+  experimental: {
+     serverComponentsExternalPackages: ['mongoose'],
+    },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+};
--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 export default async function dbConnect(){
-  await mongoose.connect('mongodb://127.0.0.1:27017/next14');
+  await mongoose.connect(process.env.MONGOURL);
 }
