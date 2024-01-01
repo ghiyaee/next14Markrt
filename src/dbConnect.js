c17b036d74c3f8bc6c45020cf33e74bf3dadc1a4
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2024-01-01 18:35:34 +0330

    more time out mongoose

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 export default async function dbConnect(){
   await mongoose.connect(process.env.MONGOURL, {
+    serverSelectionTimeoutMS: 20000,
   });
   console.log('connect db');
 }
