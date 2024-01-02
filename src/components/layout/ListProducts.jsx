daac77a0a770a2dde99968c4ed0f9e17bb57dc8f
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2024-01-02 12:01:54 +0330

    update send data

--- src/components/layout/ListProducts.jsx
+++ src/components/layout/ListProducts.jsx
@@ -6,8 +6,8 @@ import Link from 'next/link';
 import data from '../../../data';
 async function ListProducts() {
   await dbConnect();
-  await Product.deleteMany({})
-  await Product.insertMany(data.products)
+  // await Product.deleteMany({})
+  // await Product.insertMany(data.products)
   const products = await Product.find();
   return (
     <section className="flex  md:flex-row justify-center flex-wrap gap-10 mt-0">
