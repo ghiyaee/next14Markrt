dde59f19cc757c97c9722c4630e6b25560b8563c
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2024-01-03 17:35:33 +0330

    update category

--- src/components/layout/CategorySlice.jsx
+++ src/components/layout/CategorySlice.jsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 async function CategorySlice({ searchParams }) {
   let products;
   try {
-     products = await Product.find({ brand: searchParams.value });
+    products = await Product.find({ brand: searchParams.value });
+    console.log(products);
   } catch (error) {
     console.error('error singel product',error);
   }
