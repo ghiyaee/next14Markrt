18bb60c7c01a593bfe7c173b1db5525c8054fb1d
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2024-01-05 00:08:35 +0330

    update dbConnect

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -3,7 +3,7 @@ async function dbConnect() {
   try {
     if (!process.env.MONGOURL)
       console.log('Failed to read environment variables');
-    await mongoose.connect(process.env.MOGOURL);
+    await mongoose.connect(process.env.MONGOURL);
     console.log('connect db');
   } catch (error) {
     console.error('error connect to db', error);
